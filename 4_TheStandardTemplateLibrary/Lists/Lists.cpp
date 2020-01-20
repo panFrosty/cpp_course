7717d45022d3dc368dacbff35eec7957be607b87
Piotr Górny <kartoniasty@gmail.com>
2020-01-20 12:30:40 +0100

    Add Lists
    Modified CMakeLists

---
+++ 4_TheStandardTemplateLibrary/Lists/Lists.cpp
@@ -0,0 +1,54 @@
+/**
+ * @title: Lists
+ * @author: pgorny
+ * 
+ **/
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list<int> numbers;
+    numbers.push_back(1);
+    numbers.push_back(2);
+    numbers.push_back(3);
+    numbers.push_front(0);
+
+    list<int>::iterator it = numbers.begin();
+    it++;
+    numbers.insert(it, 100);
+    cout << "Element in list: " << *it << endl;
+
+    list<int>::iterator eraseIt = numbers.begin();
+    eraseIt++;
+    eraseIt = numbers.erase(eraseIt);
+    cout << "Element in list: " << *eraseIt << endl;
+
+    for (list<int>::iterator it = numbers.begin(); it != numbers.end();)
+    {
+        if (*it == 2)
+        {
+            numbers.insert(it, 1234);
+        }
+        if (*it == 1)
+        {
+            it = numbers.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
+
+    
+
+    for (list<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
