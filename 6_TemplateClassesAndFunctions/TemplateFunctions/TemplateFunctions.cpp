f1304d104b19a39c41ce82bf0b21aa74c47e18b3
Piotr Górny <kartoniasty@gmail.com>
2020-02-14 10:49:35 +0100

    Add TemplateFunctions

--- 6_TemplateClassesAndFunctions/TemplateFunctions/TemplateFunctions.cpp
+++ 6_TemplateClassesAndFunctions/TemplateFunctions/TemplateFunctions.cpp
@@ -14,9 +14,19 @@ void print(T n)
     cout << n << endl;
 }
 
+void print(const char* buffer)
+{
+    cout << "Buffer: " << buffer << endl; 
+}
+
 
 int main()
 {
     print<string>("Hello");
+    print<int>(5);
+
+    print("Hello there");
+    const char * TEST_STRING = "Test";
+    print(TEST_STRING);
     return 0;
 }
